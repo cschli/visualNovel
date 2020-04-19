23a1802cdc395bdf43112f0b9a3629a488726681
cli <cli@mastercontrol.com>
2020-04-19 15:58:29 -0600

    committing working prototype

---
+++ js/basicPage.js
@@ -0,0 +1,15 @@
+function startNewGame() {
+    console.log("Placeholder: Start new game");
+    console.log("Placeholder: Remove any existing game from memory");
+    console.log("Placeholder: Load game page");
+}
+
+function loadGame() {
+    console.log("Placeholder: Load existing game in memory");
+    console.log("Placeholder: Load game page");
+}
+
+function goToSetting() {
+    console.log("Placeholder: Load settings page");
+    
+}
